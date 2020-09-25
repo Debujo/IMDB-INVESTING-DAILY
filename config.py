9bb180efbc43d101fa836530135f26b55297e37d
Ignacio <ignaciomdb@hotmail.com>
2020-09-25 22:35:20 +0200

    Previo a hacer funciones

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+   ___                 __  _
+  / __\  ___   _ __   / _|(_)  __ _
+ / /    / _ \ | '_ \ | |_ | | / _` |
+/ /___ | (_) || | | ||  _|| || (_| |
+\____/  \___/ |_| |_||_|  |_| \__, |
+                              |___/
+"""
+# From which date you want to obtain the data
+from_date = "01/01/2018"
+# Number of days to calculate the mean
+mean_number =  45
+# Country where the stocks you want are located
+country_name = 'United States'
+# Saves all data in different csv (True / False)
+csv_save = False
+# Location of the saved csv_save
+data_location = 'C:/Users/ignac/pyprojects/Investing_daily/Data/'
--- get_data.py
+++ get_data.py
@@ -1,28 +1,36 @@
 import investpy
 import datetime
 import pandas as pd
-import time
+import time, config, os
 
 today = datetime.date.today()
 date = str(today.day) + "/" + str(today.month) + "/" + str(today.year)
-from_date = str(today.day) + "/" + str(today.month) + "/" + str(today.year-2)
-mean_date = str(today.day) + "/" + str(today.month-1) + "/" + str(today.year)
-print("Today is: " +  date)
+mean_datetime = today - datetime.timedelta(days = config.mean_number)
+mean_date = str(mean_datetime.day) + "/" + str(mean_datetime.month) + "/" + str(mean_datetime.year)
 
-stock = []
-stocks = investpy.get_stocks_list(country = 'United States')
+stocks = investpy.get_stocks_list(country = config.country_name)
 #print(len(stocks)) #4582*25kb = 120Mb en csv
 
+stock = []
 for x in stocks:
     try:
-        stock = investpy.get_stock_historical_data(stock= x, country='United States', from_date=from_date, to_date=date)
-        #print(str(x))
-        #media = mean(stock.tail()[:,4])
-        media = stock.mean(axis=0)
-        print(media)
+        stock = investpy.get_stock_historical_data(stock= x, country=config.country_name, from_date=config.from_date, to_date=date)
+        print(str(x))
+        n_days = len(stock)
+        mean_df = stock[n_days-config.mean_number:n_days] # Filters the Data Frame to get the last mean_number days
+        simple_mean = mean_df.Close.mean()
+        print(simple_mean)
+
         name = str(x + "_data.csv")
-        stock.to_csv('C:/Users/ignac/pyprojects/Investing_daily/Data/' + name)
-        sleep(0.5)
+        if config.csv_save == True:
+            stock.to_csv(config.data_location + name)
+            sleep(0.5)
+        else:
+            try:
+                os.remove(config.data_location + name)
+            except OSError:
+                pass
+
 
     except:
         pass
